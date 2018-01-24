e9655f779ce1f5929085579be25e94c7eb5eae45
Nyovelt <Nyovelt@users.noreply.github.com>
2018-01-24 15:54:52 +0800

    1

--- 外部端/putOut/putOut.ino
+++ 外部端/putOut/putOut.ino
@@ -1,25 +1,21 @@
-//Arduino Uno
-//(PIR Motion Sensor
-//It's just an example
-//So WTF u want
-
 const int PIRSensor = 2; //PIRsensor'S PIN
 int sensorValue; //PIR's return
 void setup() {
   // put your setup code here, to run once:
   pinMode (PIRSensor ,INPUT);
-  Serial.begin(9600);
+  Serial.begin(9600);//Bluetooth&&PCSerial
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensorValue = digitalRead(PIRSensor);
-  delay(1000);
-  if(sensorValue == HIGH){ //ON
+  delay(3000);//+3S
+  /*if(sensorValue == HIGH){ //ON
     Serial.println("ON");
+    Serial.println('X');
   }
   else{
-    Serial.println("OFF");
-    
-  }
+    Serial.println("OFF");//OFF
+  }*/
+  (sensorValue == HIGH)?Serial.println('X'):Serial.println("OFF");
 }
